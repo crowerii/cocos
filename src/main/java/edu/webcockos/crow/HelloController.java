d491c7f1e0babd1a941f821f3e09f7ef67a94a49
crowerii <crower.v@gmail.com>
2020-07-15 00:04:50 +0200

    start

--- src/main/java/edu/webcockos/crow/HelloController.java
+++ src/main/java/edu/webcockos/crow/HelloController.java
@@ -1,11 +1,13 @@
 package edu.webcockos.crow;
 
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
-    public class HelloWorld {
-        public static void main(String[] args) {
-            Greeter greeter = new Greeter();
-            System.out.println(greeter.sayHello());
-        }
-    }
+
+
+
+
 
 }
