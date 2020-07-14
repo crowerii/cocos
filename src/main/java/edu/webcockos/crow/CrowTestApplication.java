6cb9a50ae524d9a3953252b9bb8ed7511e9b6e8a
crowerii <crower.v@gmail.com>
2020-07-14 23:04:21 +0200

    start

---
+++ src/main/java/edu/webcockos/crow/CrowTestApplication.java
@@ -0,0 +1,13 @@
+package edu.webcockos.crow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowTestApplication.class, args);
+	}
+
+}
