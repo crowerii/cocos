79b2361f55963a9be5522da7b8afac6c72092c08
crowerii <crower.v@gmail.com>
2020-07-15 00:13:56 +0200

    start

--- src/main/java/edu/webcockos/crow/HelloController.java
+++ src/main/java/edu/webcockos/crow/HelloController.java
@@ -1,10 +1,15 @@
 package edu.webcockos.crow;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @RequestMapping("/")
+    public String index(){
+        return "This is my first web page";
+    }
 
 
 
