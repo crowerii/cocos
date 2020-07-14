9b298044fed7571182bed837df592242fec925e8
crowerii <crower.v@gmail.com>
2020-07-14 23:46:02 +0200

    start

---
+++ src/main/java/edu/webcockos/crow/HelloController.java
@@ -0,0 +1,11 @@
+package edu.webcockos.crow;
+
+public class HelloController {
+    public class HelloWorld {
+        public static void main(String[] args) {
+            Greeter greeter = new Greeter();
+            System.out.println(greeter.sayHello());
+        }
+    }
+
+}
